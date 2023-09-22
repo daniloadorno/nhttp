0dc6d3b65728cf63f905dd0e55dcde1419398b07
daniloadorno <daniloadorno@gmail.com>
2023-09-21 23:31:29 -0300

    adjusts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,11 @@
 * First Release.
 
 ## [1.0.1] - Adjusts
+
+* First Release.
+
+## [1.0.2] - Adjusts
+
+## [1.0.3] - Adjusts
+
+## [1.0.4] - Adjusts
--- test/nhttp_test.dart
+++ test/nhttp_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nhttp/nhttp.dart' as nhttp;
 
 void main() {
   test('test', () async {});
